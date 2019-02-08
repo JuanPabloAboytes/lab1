34b77ce2ebfc0462745a801f200d44c355fd685b
Juan Pablo Aboytes <a01701249@itesm.mx>
2019-02-08 15:44:08 -0600

    Agregar video, agregar landscapes a ambas actividades Paso por parametro el input y lo lleva a otra actividad Uso de imagen

--- app/src/main/java/com/example/juanpablo/lab1jpan/TargetActivity.java
+++ app/src/main/java/com/example/juanpablo/lab1jpan/TargetActivity.java
@@ -1,10 +1,13 @@
 package com.example.juanpablo.lab1jpan;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class TargetActivity extends AppCompatActivity {
 
@@ -16,6 +19,14 @@ public class TargetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_target);
 
         tv1 = (TextView)findViewById(R.id.tv1);
+        VideoView videoView = findViewById(R.id.videoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.elmo;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
         String data = getIntent().getStringExtra("data");
         //Colocarlo dentro de nuestro tv1 que es nuestro text view que tengo
         tv1.setText("Hello "+ data);
